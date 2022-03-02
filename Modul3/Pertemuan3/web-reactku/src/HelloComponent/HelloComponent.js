2b3caf119237390d726bbfcf66cbf9237c77946e
Ilham Ibrahim <54558904+ilhamibrahim-id@users.noreply.github.com>
2022-03-02 10:51:26 +0700

    Pengerjaan selesai Modul3

---
+++ Modul3/Pertemuan3/web-reactku/src/HelloComponent/HelloComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './HelloComponent.css';
+
+const HelloComponent = () =>{
+    return <p className='test-p'> Saya adalah ilham ibrahim</p>
+}
+
+export default HelloComponent;
---
+++ Modul3/Pertemuan3/web-reactku/src/Login/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div class="body_top">
+            <h2>Form Login</h2>
+            <div class="body_login">
+                <h1>Tugas Pertemuan <br />Ketiga</h1>
+                <form>
+                    <div class="body_input">
+                        <label for="fname" class="nameinput" >Username</label>
+                        <input placeholder="Masukan Username"></input>
+                        <label for="fname" class="namepw">Password</label>
+                        <input placeholder="Masukan Password Anda"></input></div>                                                
+                    <button class="button button1">Login</button>                                            
+                </form>
+                <input type="checkbox"></input><label class="checkbox">Remember Me</label>
+                <br/>
+                <button class="button_bawah button2">Cancel</button>                                                            
+            </div>
+        </div>
+
+    );
+}
+
+export default Login;
