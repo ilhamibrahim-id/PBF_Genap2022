133812179ce1788a20b8cc704290c8810b733c2e
Ilham Ibrahim <54558904+ilhamibrahim-id@users.noreply.github.com>
2022-03-16 09:40:30 +0700

    Selesai sampai praktikum 4 | tugas not yet

--- Modul4/web-reactku/src/container/BlogPost/BlogPost.jsx
+++ Modul4/web-reactku/src/container/BlogPost/BlogPost.jsx
@@ -1,35 +1,93 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import './BlogPost.css';
 import Post from "../../component/BlogPost/Post";
-import { render } from "react-dom";
 
-class BlogPost extends Component {
+class BlogPost extends Component{
     state = {
-        listArtikel: []
+        listArtikel:[],
+        insertArtikel: {
+           userId: 1,
+           id: 1,
+           title: "",
+           body:""
+        }
+    }
+
+    ambilDataDariServerAPI = () => {
+        fetch(`http://localhost:3001/posts?_sort=id&_order=desc`)
+        .then(response => response.json())
+        .then(jsonHasilAmbilDariAPI =>{
+            this.setState({
+                listArtikel: jsonHasilAmbilDariAPI
+            })
+        })
+        
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(jsonHasilAmbilDariAPI => {
-                this.setState({
-                    listArtikel: jsonHasilAmbilDariAPI
-                })
+        this.ambilDataDariServerAPI()
+    }
+
+    handleHapusArtikel = (data) => {
+        fetch(`http://localhost:3001/posts/${data}`, {method: 'DELETE'})
+            .then(res => { 
+                this.ambilDataDariServerAPI()
             })
     }
 
+    handleTambahArtikel = (event) =>{
+        let formInsertArtikel = {...this.state.insertArtikel};
+        let timestamp = new Date().getTime();
+        formInsertArtikel['id'] = timestamp;
+        formInsertArtikel[event.target.name] = event.target.value;
+        this.setState({
+            insertArtikel: formInsertArtikel
+        })
+    }
 
-render(){
-    return (
-        <div className="post-artikel">
-            <h2>Daftar artikel</h2>
-            {
-                this.state.listArtikel.map(artikel => {
-                    return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} />
-                })
-            }
-        </div>
-    )
+    handleTombolSimpan = () => {
+        fetch('http://localhost:3001/posts', {
+            method:'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.insertArtikel)
+        })
+
+            .then( (response ) => {
+                this.ambilDataDariServerAPI();
+            });
+    }
+
+
+    render() {
+       return(
+           <div className="post-artikel">
+               <div className="form pb-2 border-button">
+                   <div className="form-group row">
+                       <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
+                       <div className="col-sm">
+                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
+                       </div>
+                   </div>
+                   <div className="form-group row">
+                       <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                       <div className="col-sm-10">
+                           <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                       </div>
+                   </div>
+                   <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
+               </div>
+               <h2>Daftar Artikel</h2>
+               {
+                   this.state.listArtikel.map(artikel =>{
+                       return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id}hapusArtikel={this.handleHapusArtikel}/>
+                   })
+               }
+           </div>
+       )
+    }
 }
-}
+
 export default BlogPost;
