e20182a3ce000bc4b69bbe0a2f92992c90a739af
Ilham Ibrahim <54558904+ilhamibrahim-id@users.noreply.github.com>
2022-03-23 14:18:18 +0700

    modul 5 selesai praktikum tugas belum

--- Modul5/web-reactku/src/App.js
+++ Modul5/web-reactku/src/App.js
@@ -1,25 +1,363 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+// P1
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+// export default function BasicExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/home">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/dashboard">Dashboard</Link>
+//           </li>
+//         </ul>
+
+//         <hr />
+//         <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/dashboard">
+//             <Dashboard />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// // You can think of these components as "pages"
+// // in your app.
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
+
+// P2
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useParams
+// } from "react-router-dom";
+
+// export default function ParamsExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/netflix">netflix</Link>
+//           </li>
+//           <li>
+//             <Link to="/gmail">gmail</Link>
+//           </li>
+//           <li>
+//             <Link to="/yahoo">yahoo</Link>
+//           </li>
+//           <li>
+//             <Link to="/amazon">amazon</Link>
+//           </li>
+//         </ul>
+
+//         <hr />
+//         <Switch>
+//           <Route path="/:id" children={<Child />} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// // You can think of these components as "pages"
+// // in your app.
+
+// function Child(){
+//   let { id } = useParams();
+//   return (
+//     <div>
+//       <h3>ID: {id}</h3>
+//     </div>
+//   );
+// }
+
+// P3
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useParams,
+//   useRouteMatch
+// } from "react-router-dom";
+
+// export default function NestingExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/topics">Topics</Link>
+//           </li>
+//         </ul>
+//         <hr />
+
+//         <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route path="/topics">
+//             <Topics />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
+
+// function Topics() {
+//   let { path, url } = useRouteMatch();
+
+//   return (
+//     <div>
+//       <h2>Topics</h2>
+//       <ul>
+//         <li>
+//           <Link to={`${url}/Sate, Nasi goreng`}>Kuliner</Link>
+//         </li>
+//         <li>
+//           <Link to={`${url}/Wisata alam, Museum`}>Travelling</Link>
+//         </li>
+//         <li>
+//           <Link to={`${url}/Salaris, JW Marriot`}>Review Hotel</Link>
+//         </li>
+//       </ul>
+
+//       <Switch>
+//         <Route exact path={path}>
+//           <h3>Please select a topic.</h3>
+//         </Route>
+//         <Route path={`${path}/:topicId`}>
+//           <Topic />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// function Topic() {
+//   let { topicId } = useParams();
+
+//   return (
+//     <div>
+//       <h3>{topicId}</h3>
+//     </div>
+//   );
+// }
+
+// P4
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams,
+    useRouteMatch,
+    useHistory,
+    useLocation,
+    Redirect
+  } from "react-router-dom";
+  
+export default function AuthExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <AuthButton />
+
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/private">Private Page</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/public">
+            <PublicPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/private">
+            <ProtectedPage />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+function AuthButton() {
+  let history = useHistory();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button
+        onClick={() => {
+          fakeAuth.signout(() => history.push(from));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicPage() {
+  return (
+    <div>
+      <h3>Public</h3>
     </div>
   );
 }
 
-export default App;
+function ProtectedPage() {
+  return (
+    <div>
+      <h3>Private</h3>
+    </div>
+  );
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
+
+
