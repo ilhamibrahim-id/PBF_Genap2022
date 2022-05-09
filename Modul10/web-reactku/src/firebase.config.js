d5e842d1fb27fba5da6113e841e19b3a653591f9
Ilham Ibrahim <54558904+ilhamibrahim-id@users.noreply.github.com>
2022-05-09 13:45:37 +0700

    Modul 10 push done

---
+++ Modul10/web-reactku/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBdHQBQt4aC_p3BB7gzUohm643FyU4ppNA",
+    authDomain: "batoka-68182.firebaseapp.com",
+    projectId: "batoka-68182",
+    storageBucket: "batoka-68182.appspot.com",
+    messagingSenderId: "1008374831917",
+    appId: "1:1008374831917:web:8816d7e733fa8ce586d1a4",
+    measurementId: "G-YWSH3V0DMT"
+};
