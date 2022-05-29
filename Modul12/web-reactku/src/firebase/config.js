08afd0c1de46921c8a39d155e90e2cf1eed69eb1
Ilham Ibrahim <54558904+ilhamibrahim-id@users.noreply.github.com>
2022-05-29 19:35:59 +0700

    Modul 12 push selesai

---
+++ Modul12/web-reactku/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1o4fRXjNQREChg0q2M2yvuSbADG-Us-c",
+    authDomain: "loginfirebase-14196.firebaseapp.com",
+    projectId: "loginfirebase-14196",
+    storageBucket: "loginfirebase-14196.appspot.com",
+    messagingSenderId: "464447853141",
+    appId: "1:464447853141:web:1dd82944e1e4fc971ffadc",
+    measurementId: "G-YGSG4KE8WX"
+  };
+
+export default firebaseConfig;
